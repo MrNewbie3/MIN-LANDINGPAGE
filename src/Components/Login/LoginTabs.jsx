d3a9967c269ffab3737ba8121b983a6a1878357e
kkafi09 <afi.firda@gmail.com>
2023-01-20 16:17:08 +0700

    add: merging and integrate api auth

--- src/Components/Login/LoginTabs.jsx
+++ src/Components/Login/LoginTabs.jsx
@@ -1,24 +1,83 @@
-import React from 'react'
-import LoginForm from './LoginForm';
-import TitleLogin from './TitleLogin';
+import React, { useState } from "react";
+import axios from "../../lib/axios";
+import TitleLogin from "./TitleLogin";
+import { SyncLoader } from "react-spinners";
+import { useAuthContext } from "../../hooks/useAuthContext";
+import { useNavigate } from "react-router-dom";
 
 const LoginTabs = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+  const { dispatch } = useAuthContext();
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+
+    const dataLogin = { email, password };
+
+    await axios.post("/api/auth/login", dataLogin).then((user) => {
+      localStorage.setItem("user", JSON.stringify(user.data.data));
+      console.log("user", user);
+
+      dispatch({ type: "LOGIN", payload: user.data.data.user });
+
+      setLoading(false);
+      navigate("/ppdbapps");
+    });
+
+    setLoading(false);
+  };
+
   return (
-    <> 
-    <div className="content-wrap mt-8 flex justify-center flex-col">
-      <TitleLogin desc={'Masukan email dan password yang sudah dibuat'} />
-      <div className="form-content mt-7">
-        <LoginForm label={'Email'} type={'email'} placeholder={'cth: Tommy Kurniawan'}/>
-        <LoginForm label={'Kata Sandi'} type={'password'}/>
+    <>
+      <div className="content-wrap mt-8 flex justify-center flex-col">
+        <TitleLogin desc={"Masukan email dan password yang sudah dibuat"} />
+        <form onSubmit={(e) => handleSubmit(e)}>
+          <div className="form-content mt-7">
+            <div className="form-input mb-2">
+              <p className="text-base font-medium">Email :</p>
+              <input
+                className="w-full px-4 py-2 rounded-full mt-2 bg-grey text-gray-700"
+                type={"email"}
+                placeholder={"cth: Tommy Kurniawan"}
+                onChange={(e) => setEmail(e.target.value)}
+                required
+              />
+            </div>
+            <div className="form-input mb-2">
+              <p className="text-base font-medium">Password :</p>
+              <input
+                className="w-full px-4 py-2 rounded-full mt-2 bg-grey text-gray-700"
+                type={"password"}
+                placeholder={"*******"}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+              />
+            </div>
+          </div>
+          <div className="button-login mt-4">
+            <button
+              type="submit"
+              className="py-2 w-full rounded-full bg-green-900 text-white font-semibold text-base d-flex items-center justify-center"
+            >
+              {loading && <SyncLoader size={7} color={"#fff"} />}
+              {!loading && "Login"}
+            </button>
+            <div className="w-full h-px my-3 bg-gray-300"></div>
+            <button
+              type="submit"
+              className="py-2 w-full rounded-full border-2 border-green-900  text-green-900 font-semibold text-base"
+            >
+              Lupa kata sandi
+            </button>
+          </div>
+        </form>
       </div>
-      <div className="button-login mt-4">
-        <button type='submit' className='py-2 w-full rounded-full bg-green-900 text-white font-semibold text-base'>Login</button> 
-        <div className='w-full h-px my-3 bg-gray-300'></div>
-        <button type='submit' className='py-2 w-full rounded-full border-2 border-green-900  text-green-900 font-semibold text-base'>Lupa kata sandi</button> 
-      </div>
-    </div>
     </>
-  )
-}
+  );
+};
 
 export default LoginTabs;
--- src/Components/Login/RegisterTabs.jsx
+++ src/Components/Login/RegisterTabs.jsx
@@ -1,25 +1,101 @@
-import React from 'react'
-import LoginForm from './LoginForm';
-import TitleLogin from './TitleLogin';
+import React, { useState } from "react";
+import { redirect } from "react-router-dom";
+import { SyncLoader } from "react-spinners";
+import { useAuthContext } from "../../hooks/useAuthContext";
+import axios from "../../lib/axios";
+import TitleLogin from "./TitleLogin";
 
 const RegisterTabs = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+  const { dispatch } = useAuthContext();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+
+    const newUser = {
+      nama_lengkap: name,
+      email: email,
+      password: password,
+      confirm_password: confirmPassword,
+    };
+
+    await axios.post("/api/auth/register", newUser).then((user) => {
+      localStorage.setItem("user", user.data.data.user);
+
+      dispatch({ type: "LOGIN", payload: user.data.data.user });
+
+      setLoading(false);
+      redirect("/login");
+    });
+
+    setLoading(false);
+  };
+
   return (
     <>
       <div className="content-wrap mt-8 flex justify-center flex-col">
-        <TitleLogin desc={'Buat email dan kata sandi untuk login'} />
-        <div className="form-content mt-7">
-          <LoginForm label={'Nama Siswa'} type={'text'} placeholder={'cth: Tommy Kurniawan'} />
-          <LoginForm label={'Email'} type={'email'} placeholder={'cth: tommykurniawan123@gmail.com'} />
-          <LoginForm label={'Kata sandi'} type={'password'}/>
-          <LoginForm label={'Ulangi Kata Sandi'} type={'password'}/>
-        </div>
-        <div className="button-login mt-4">
-          <button type='submit' className='py-2 w-full rounded-full bg-green-900 text-white font-semibold text-base'>Daftar</button> 
-        </div>
+        <TitleLogin desc={"Buat email dan kata sandi untuk login"} />
+        <form onSubmit={(e) => handleSubmit(e)}>
+          <div className="form-content mt-7">
+            <div className="form-input mb-2">
+              <p className="text-base font-medium">Nama siswa : </p>
+              <input
+                className="w-full px-4 py-2 rounded-full mt-2 bg-grey text-gray-700"
+                type={"text"}
+                placeholder={"cth: Tommy Kurniawan"}
+                onChange={(e) => setName(e.target.value)}
+                required
+              />
+            </div>
+            <div className="form-input mb-2">
+              <p className="text-base font-medium">Email : </p>
+              <input
+                className="w-full px-4 py-2 rounded-full mt-2 bg-grey text-gray-700"
+                type={"email"}
+                placeholder={"cth: tommykurniawan123@gmail.com"}
+                onChange={(e) => setEmail(e.target.value)}
+                required
+              />
+            </div>
+            <div className="form-input mb-2">
+              <p className="text-base font-medium">Kata sandi : </p>
+              <input
+                className="w-full px-4 py-2 rounded-full mt-2 bg-grey text-gray-700"
+                type={"password"}
+                placeholder={"********"}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+              />
+            </div>
+            <div className="form-input mb-2">
+              <p className="text-base font-medium">Ulangi Kata sandi : </p>
+              <input
+                className="w-full px-4 py-2 rounded-full mt-2 bg-grey text-gray-700"
+                type={"password"}
+                placeholder={"********"}
+                onChange={(e) => setConfirmPassword(e.target.value)}
+                required
+              />
+            </div>
+          </div>
+          <div className="button-login mt-4">
+            <button
+              type="submit"
+              className="py-2 w-full rounded-full bg-green-900 text-white font-semibold text-base"
+            >
+              {loading && <SyncLoader size={7} color={"#fff"} />}
+              {!loading && "Daftar"}
+            </button>
+          </div>
+        </form>
       </div>
-      
     </>
-  )
-}
+  );
+};
 
 export default RegisterTabs;
